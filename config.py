857c9896065d3f585c7403a25421b3f47fbff70a
kingold5 <xingran_wang@icloud.com>
2020-04-16 21:47:58 +0200

    use utf8mb4 for emoji

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 # pylint: disable=missing-docstring,too-few-public-methods,invalid-name,line-too-long,wrong-import-order
 class Config():
     # Config MySQL and secret key
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '5515660'
-    MYSQL_DB = 'myflaskapp'
-    MYSQL_CURSORCLASS = 'DictCursor'
     SECRET_KEY = 'secret_my_flask_key'
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:5515660@localhost/myflaskapp'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:5515660@localhost/myflaskapp?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
