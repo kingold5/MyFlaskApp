598239e8a32951e95e1d69ec8ca27a087dabfa4b
kingold5 <xingran_wang@icloud.com>
2020-04-17 17:47:56 +0200

    secure SECRET_KEY

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 # pylint: disable=missing-docstring,too-few-public-methods,invalid-name,line-too-long,wrong-import-order
+import os
+
+
 class Config():
     # Config MySQL and secret key
-    SECRET_KEY = 'secret_my_flask_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_my_flask_key')
 
+    # Use utf8mb4
     SQLALCHEMY_DATABASE_URI = 'mysql://root:5515660@localhost/myflaskapp?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
