f91d61b784a2390aeee6a5ae092be1d1fd69a017
kingold5 <xingran_wang@icloud.com>
2020-04-27 02:09:26 +0200

    add followers association, follow(), unfollow(), is_following().

--- myflask/models.py
+++ myflask/models.py
@@ -6,6 +6,13 @@ from flask_login import UserMixin
 from myflask import db, login
 
 
+# Followers association table
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
+)
+
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.VARCHAR(100), nullable=True)
@@ -17,6 +24,11 @@ class Users(UserMixin, db.Model):
     about_me = db.Column(db.String(191), nullable=True)
     password = db.Column(db.VARCHAR(128), nullable=True)
     last_seen = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    # Set followers
+    followed = db.relationship('Users', secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
     articles = db.relationship('Articles', backref='author', lazy='dynamic')
     register_date = db.Column(db.TIMESTAMP, nullable=False, default=datetime.datetime.utcnow)
 
@@ -32,6 +44,43 @@ class Users(UserMixin, db.Model):
     def avatar(self, size=80):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def follow(self, user):
+        """If not following, follow the user
+
+        user : Class Users
+            user candidiate to be followed
+        """
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        """If followed, unfollow the user
+
+        Parameters
+        ----------
+        user : Class Users
+            user candidate to be unfollowed
+        """
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        """Check if following the user
+
+        Parameters
+        ----------
+        user : Class Users
+
+        Returns
+        -------
+        boolean
+            True : user is followed
+            False : user is not followed
+        """
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
 
 class Articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
